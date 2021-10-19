0cf8a9daf9a16730659a788c941f5094cc250a37
xlv <xaljavamail@gmail.com>
2021-10-19 13:00:40 +0800

    Initial Commit

---
+++ hwsb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hwsb
+//
+//  Created by Vlad Zavada on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
